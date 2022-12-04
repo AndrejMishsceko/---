1fc9bc1e26dc0efc72769e0f774b5a4d7701602e
Andropoles <Andropoles@.bk.ru>
2022-12-04 16:27:25 +0400

    5

---
+++ 5 семинар/homework for seminar №4.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `ИНФОРМАЦИЯ ОБ ОДНОГРУППНИКАХ` (
+    ID INT (10) AUTO_INCREMENT,
+    NAME VARCHAR(20) NOT NULL,
+    AGE VARCHAR(50) NOT NULL,
+    ADDRESS VARCHAR(15) NOT NULL,
+    PRIMARY KEY (ID)
+);
---
+++ 5 семинар/in.php
@@ -0,0 +1,11 @@
+<?php
+    $profession = 'Токарь';
+    $city = 'Самара';
+    $email = 'Googl.com ';
+    $mobile = '12345678999999 ';
+    &worke = 'dgarg';
+    &hdhh = 
+    
+
+    echo $_профессия
+?>Front End Developer
